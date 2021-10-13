3e76cc635338c7ac34fbc276c50fb8a6d6506f53
QA-AutomationTesting <use.info02@gmail.com>
2021-10-12 20:36:06 -0800

    Selenium Integration Test

---
+++ src/test/java/MainClass.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = null;
+		WebDriverManager.chromedriver().browserVersion("77.0.3865.40").setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("enable-automation");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-infobars");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-browser-side-navigation");
+		options.addArguments("--disable-gpu");
+		driver = new ChromeDriver(options);
+		driver.get("https://www.google.com/");
+	}
+}
